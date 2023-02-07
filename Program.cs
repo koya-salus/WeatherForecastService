16af3cf642fb3e7cba39556e22589b605437bb5a
abiola-deimos <107115160+abiola-deimos@users.noreply.github.com>
2023-02-07 12:59:58 +0100

    fix app

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-break now;
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
