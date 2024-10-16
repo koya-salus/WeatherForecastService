b1b0e4a030076fc6d3dd08129aeb91c344e172cd
Olamide Onakoya <123734265+koya-deimos@users.noreply.github.com>
2024-10-16 15:21:38 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ if (app.Environment.IsDevelopment())
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchingg"
 };
 
 app.MapGet("api/v2", (ILogger<Program> logger) =>
