e81f7d28e22de327791e1a9e4e2e56d498fb6fe0
Olamide Onakoya <123734265+koya-deimos@users.noreply.github.com>
2025-01-08 11:21:30 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -71,7 +71,24 @@ app.MapGet("api/v2/countries", (ILogger<Program> logger) =>
     </html>
     ", "text/html");
 });//.Produces(200, contentType: "text/html");
-    
+
+app.MapGet("api/v2/error", (ILogger<Program> logger) =>
+{
+    logger.LogError("errors page");
+    var now = DateTime.UtcNow;
+    return Results.Text(@$"
+    <html>
+    <head>
+    <link rel='stylesheet' href='https://cdn.simplecss.org/simple-v1.css'>
+    </head>
+    <body>
+    <h1> Error Log at {now.ToUniversalTime().ToString()}  </h1>
+    <p>The time now in UTC is  </p>
+    </body>
+    </html>
+    ", "text/html");
+});
+
 app.MapGet("api/v2/random", (ILogger<Program> logger) =>
 {
     logger.LogInformation("random page");
